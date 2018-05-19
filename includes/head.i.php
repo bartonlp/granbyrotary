6b3797f885409337e1742b4d84a8a3538e69557b
Barton Phillips <bartonphillips@gmail.com>
2018-05-19 06:22:45 -0700

    Add 'NO GOOD MSIE' modified:   includes/head.i.php

--- includes/head.i.php
+++ includes/head.i.php
@@ -1,4 +1,37 @@
 <?php
+// BLP 2018-05-11 -- ADD 'NO GOOD MSIE' message
+
+if(!$this->isBot) {
+  if(preg_match("~^.*(?:(msie\s*\d*)|(trident\/*\s*\d*)).*$~i", $this->agent, $m)) {
+    $which = $m[1] ? $m[1] : $m[2];
+    echo <<<EOF
+<!DOCTYPE html>
+<html>
+<head>
+  <title>NO GOOD MSIE</title>
+</head>
+<body>
+<div style="background-color: red; color: white; padding: 10px;">
+Your browser's <b>User Agent String</b> says it is:<br>
+$m[0]<br>
+Sorry you are using Microsoft's Broken Internet Explorer ($which).</div>
+<div>
+<p>You should upgrade to Windows 10 and Edge if you must use MS-Windows.</p>
+<p>Better yet get <a href="https://www.google.com/chrome/"><b>Google Chrome</b></a>
+or <a href="https://www.mozilla.org/en-US/firefox/"><b>Mozilla Firefox</b>.</p></a>
+These two browsers will work with almost all previous
+versions of Windows and are very up to date.</p>
+<b>Better yet remove MS-Windows from your
+system and install Linux instead.
+Sorry but I just can not continue to support ancient versions of browsers.</b></p>
+</div>
+</body>
+</html>
+EOF;
+    exit();
+  }
+}
+  
 return <<<EOF
 <head>
   <title>{$arg['title']}</title>
